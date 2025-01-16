3becbeed23f66945e99783825e6a935d3ebc61e5
MielnickiB <mielnicki.bartek@gmail.com>
2025-01-16 20:19:21 +0100

    Added map component for facility record page

--- force-app/main/default/lwc/medicalFacilityMap/medicalFacilityMap.js
+++ force-app/main/default/lwc/medicalFacilityMap/medicalFacilityMap.js
@@ -1,33 +1,35 @@
-import { LightningElement, wire, track } from 'lwc';
-import getMedicalFacilityData from '@salesforce/apex/MedicalFacilityController.getMedicalFacilityData';
+import { LightningElement, wire, api } from 'lwc';
+import getMedicalFacility from '@salesforce/apex/MedicalFacilityController.getMedicalFacility';
 
 export default class MedicalFacilityMap extends LightningElement {
-    @track medicalFacilities = [];
-    @track error;
-    @track mapMarkers = [];
-    @track center;
+    @api recordId;
+    mapMarkers = [];
 
-    @wire(getMedicalFacilityData)
-    wiredFacilities({ error, data }) {
-        if (data) {
-            this.medicalFacilities = data;
-            this.mapMarkers = data.map(facility => ({
-                location: {
-                    Street: facility.Address__Street__s,
-                    City: facility.Address__City__s,
-                    PostalCode: facility.Address__PostalCode__s
+    get hasMarkers() {
+        console.log(this.mapMarkers.length);
+        return this.mapMarkers.length > 0;
+    }
+
+    @wire(getMedicalFacility, {facilityId : '$recordId'})
+    wiredMap({ error, data }) {
+        console.log(this.recordId);
+        if(data) {
+            console.log(data);
+            this.mapMarkers = [
+                {
+                    location: {
+                        Street: data.Address__Street__s,
+                        City: data.Address__City__s,
+                        PostalCode: data.Address__PostalCode__s
+                    },
+                    title: data.Name,
+                    description:
+                        data.Address__City__s + ', ul.' + data.Address__Street__s + '. Kod pocztowy: ' + data.Address__PostalCode__s,
                 },
-                title: facility.Name
-            }));
-
-            if (this.mapMarkers.length > 0) {
-                this.center = this.mapMarkers[0].location;
-            }
-            this.error = undefined;
-        } else if (error) {
-            this.error = error.body.message;
-            this.medicalFacilities = undefined;
-            this.mapMarkers = [];
+            ];
+        }
+        else if(error) {
+            console.error(error);
         }
     }
 }
